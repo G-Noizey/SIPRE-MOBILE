553109f56237c4b98a3f86f6b8d317bc079846ee
Martinez Valladares Jair Antonio <20223tn068@utez.edu.mx>
2024-04-15 02:49:13 -0600

    Modal para comentarios

--- src/components/RecentMovements.jsx
+++ src/components/RecentMovements.jsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Button, TextInput } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import axios from 'axios'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { API_URL } from '@env'
 import { useFocusEffect } from '@react-navigation/native';
+import Modal from 'react-native-modal';
 
 export default function RecentMovements() {
 
     const [movements, setMovements] = useState([]);
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [selectedMovement, setSelectedMovement] = useState(null);
 
     const fetchMovements = async () => {
 
@@ -21,7 +24,6 @@ export default function RecentMovements() {
 
                 const responseTransferencias = await axios.get(`${API_URL}/transfer/all/${workerId}`);
                 const responseCompras = await axios.get(`${API_URL}/buys/all/${workerId}`);
-
 
                 let counter = 0;
                 const transferenciasConContador = responseTransferencias.data.map(transferencia => ({
@@ -58,25 +60,32 @@ export default function RecentMovements() {
         }, [])
     );
 
+    const handleOpenModal = (movement) => {
+        setSelectedMovement(movement);
+        setModalVisible(true);
+    };
+
+    const handleCloseModal = () => {
+        setModalVisible(false);
+    };
+
     const renderMovements = ({ item }) => (
 
         <View style={styles.container}>
 
             <View style={styles.column}>
                 <Text style={{ fontWeight: "bold" }}>Registro de </Text>
-                <Text style={{ fontWeight: "bold" }}>Concepto:</Text>
                 <Text style={{ fontWeight: "bold" }}>Cantidad:</Text>
-                <Text style={{ fontWeight: "bold" }}>Fecha:</Text>
                 <Text style={{ fontWeight: "bold" }}>Estatus:</Text>
             </View>
 
-            <View style={styles.column2}>
-                <Text style={{ fontWeight: "bold" }}>{item.tipo}:</Text>
-                <Text style={{ width: 230 }}>{item.descripcion}</Text>
-                <Text>${item.monto}</Text>
-                <Text>{item.fecha}</Text>
-                <Text>{item.status}</Text>
-            </View>
+            <TouchableOpacity onPress={() => handleOpenModal(item)}>
+                <View style={styles.column2}>
+                    <Text style={{ fontWeight: "bold" }}>{item.tipo}:</Text>
+                    <Text>${item.monto}</Text>
+                    <Text>{item.status}</Text>
+                </View>
+            </TouchableOpacity>
 
             <View style={styles.column3}>
                 {item.tipo === 'Transferencia' ? (
@@ -100,6 +109,47 @@ export default function RecentMovements() {
                     <Text style={styles.noMovementsMessage}>Sin movimientos recientes</Text>
                 )}
             />
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={isModalVisible}
+                onRequestClose={handleCloseModal}
+            >
+
+                <View style={styles.modalOverlay}>
+                    <View style={styles.modalContent}>
+                        {selectedMovement && (
+                            <>
+                                <Text>
+                                    <Text style={styles.boldText}>Registro de </Text>
+                                    <Text style={styles.boldText}>{selectedMovement.tipo}</Text>
+                                </Text>
+                                <Text>
+                                    <Text style={styles.boldText}>Concepto: </Text>
+                                    <Text>{selectedMovement.descripcion}</Text>
+                                </Text>
+                                <Text>
+                                    <Text style={styles.boldText}>Monto: </Text>
+                                    <Text>${selectedMovement.monto}</Text>
+                                </Text>
+                                <Text>
+                                    <Text style={styles.boldText}>Fecha: </Text>
+                                    <Text>{selectedMovement.fecha}</Text>
+                                </Text>
+                                <Text>
+                                    <Text style={styles.boldText}>Estado: </Text>
+                                    <Text>{selectedMovement.status}</Text>
+                                </Text>
+                                <View style={styles.commentBox}>
+                                    <Text>{selectedMovement.comentario}</Text>
+                                </View>
+                                <Button title="Cerrar" onPress={handleCloseModal} />
+                            </>
+                        )}
+                    </View>
+
+                </View>
+            </Modal>
         </View>
     );
 }
@@ -140,6 +190,49 @@ const styles = StyleSheet.create({
         marginTop: 155,
         fontSize: 20,
         color: 'gray',
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        padding: 10,
+        margin: 10,
+    },
+    modalOverlay: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        top: 0,
+        left: 0
+    },
+    modalContent: {
+        width: '80%',
+        padding: 20,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    commentBox: {
+        marginTop: 10,
+        padding: 10,
+        backgroundColor: '#f9f9f9',
+        borderColor: '#ddd',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginBottom: 10,
+    },
+    boldText: {
+        fontWeight: 'bold',
     },
 
 })
